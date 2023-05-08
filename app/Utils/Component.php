7a7ad612f08338417dcdfd870431f3f39257470e
Julimar Jr <jjunior@xcapitalbank.com.br>
2023-05-08 20:19:32 -0300

    Components

--- app/Utils/Component.php
+++ app/Utils/Component.php
@@ -1,29 +1,14 @@
 <?php
     namespace App\Utils;
 
-    use \App\Utils\View;
-
-    class Component {
-        private static $vars = [];
-        
-        private static function getContentComponent($view) {
-            $file = __DIR__ . '/../../resources/components/' . $view . '.html';
+    class Component extends View {
+        private static function getContentComponent($component) {
+            $file = __DIR__ . '/../../resources/components/' . $component . '.html';
             return file_exists($file) ? file_get_contents($file) : '';
         }
-
-        public static function render($view, $vars = []) {
-            if(empty(self::$vars))
-                self::$vars = View::getInitVars();
-
-            $contentView = self::getContentComponent($view);
-
-            $vars = array_merge(self::$vars, $vars);
-
-            $keys = array_keys($vars);
-            $keys = array_map(function($item) {
-                return '{{' . $item . '}}';
-            }, $keys);
-
-            return str_replace($keys, array_values($vars), $contentView);
+        
+        public static function render($component, $vars = []) {
+            $contentComponent = self::getContentComponent($component);
+            return parent::render(false, $vars, $contentComponent);
         }
     }
